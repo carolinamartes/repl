8ef0450fa0784a339bc78575e47ce9afa8e3e5ca
Carolina Martes <carolina.martes@gmail.com>
2016-09-26 20:53:17 -0400

    Add styles

--- public/assets/javascript/complexity.js
+++ public/assets/javascript/complexity.js
@@ -1,8 +1,7 @@
-function add(){
-var sum = 0;
-for (var i = 0; i < arguments.length; i++) {
-sum += arguments[i];
+function add(arguments){
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
 }
-return sum;
-}
-        
